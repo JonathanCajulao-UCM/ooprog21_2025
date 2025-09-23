10b256485a162ec8fda13685f8e744339247144a
JonathanCajulao-UCM <jonathancajulao26@gmail.com>
2025-09-23 08:33:34 +0800

    Add files via upload

---
+++ Chapter_4/DebugBox.java
@@ -0,0 +1,38 @@
+public class DebugBox
+{
+private int width;
+private int length;
+private int height;
+public DebugBox()
+{
+length = 1;
+width = 1;
+height = 1;
+}
+public DebugBox(int width, int length, int height)
+{
+this.width = width;
+this.length = length;
+this.height = height;
+}
+public void showData()
+{
+System.out.println("Width: " + width + "\n" + "Length: " +
+length + "\n" + "Height: " + height);
+}
+public double getVolume()
+{
+double vol = length * width * height;
+System.out.println("Volume: " + vol);
+return vol;
+}
+
+  public static void main(String[] a) {
+        DebugBox box = new DebugBox(4, 8, 4);
+        
+        box.showData();
+        box.getVolume();
+
+
+}
+}
