e41e2c8deaede8e2df858f49cd113381d50fbf19
JonathanCajulao-UCM <jonathancajulao26@gmail.com>
2025-10-23 21:25:32 +0800

    Add files via upload

---
+++ chapter_6/BankBalance2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BankBalance2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double balance = 103.8; // starting balance
+        final double INTEREST_RATE = 0.03;
+        int year = 1;
+        int choice;
+
+        do {
+            year++;
+            balance = balance + (balance * INTEREST_RATE);
+            System.out.printf("After year %d at %.2f interest rate, balance is $%.4f%n",
+                    year, INTEREST_RATE, balance);
+
+            System.out.print("\nDo you want to see the balance at the end of another year?\n"
+                    + "Enter 1 for yes\n"
+                    + "or any other number for no >> ");
+            choice = input.nextInt();
+        } while (choice == 1);
+    }
+}
